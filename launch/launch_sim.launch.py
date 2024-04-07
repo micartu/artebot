9c340a06b9fe33de66a9090d84de70a88173a71e
Michael Artuerhof <michael.artuerhof@gmail.com>
2024-04-07 09:59:26 +0300

    - allow launching in Gazebo with simulation (last part before moving to my robot dimensions)

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -14,16 +14,25 @@ def generate_launch_description():
 
     # include rsp.launch.py file
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+            PythonLaunchDescriptionSource(
+                [
+                    os.path.join(
+                        get_package_share_directory(package_name),'launch','rsp.launch.py'
+                        )
+                    ]
+                ),
+            launch_arguments={'use_sim_time': 'true'}.items()
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+            PythonLaunchDescriptionSource(
+                [
+                    os.path.join(
+                        get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+                    ]
+                ),
+    )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't
     # really matter if you only have a single robot.
